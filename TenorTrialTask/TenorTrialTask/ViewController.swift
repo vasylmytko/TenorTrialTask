16b5deee80e597a49cee00c41b474c46cbb9ba4b
Vasyl Myko <vmytko@binariks.com>
2022-06-17 20:05:30 +0300

    Create project

---
+++ TenorTrialTask/TenorTrialTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TenorTrialTask
+//
+//  Created by Vasyl Mytko on 17.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
